23b10dc5296034f0cb81c72150a4aeac26c04ff0
AntonovDev <antonov-dev@mail.ru>
2021-01-07 22:29:18 +0300

    test

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-import ReactButton from 'react-button-test-package'
+import ReactButton from 'react-button-test-package';
 
 function App() {
   return (
-    <div>
-      <ReactButton text='111' classString='hello' onClick={() => console.log(111)} />
-    </div>
-  )
+    <ReactButton
+      text='111'
+      classString='hello'
+      onClick={() => console.log(111)}
+    />
+  );
 }
 
-export default App
+export default App;
